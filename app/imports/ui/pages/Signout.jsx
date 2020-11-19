1a861bf9311711a72ff546d38e83f2aeef3f0859
keekss <keeksslss@gmail.com>
2020-11-18 22:20:12 -1000

    Aesthetic changes to signout page

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Header } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     Meteor.logout();
     return (
-      <Header id="signout-page" as="h2" textAlign="center">
-        <p>You are signed out.</p>
-      </Header>
+        <div className="landing-background">
+          <Header id="signout-page" as="h2" textAlign="center">
+            <Header as='h1' inverted>You are signed out.</Header>
+          </Header>
+        </div>
     );
   }
 }
