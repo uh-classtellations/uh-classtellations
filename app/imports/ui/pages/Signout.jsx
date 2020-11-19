872f14b412a2208aedeba9d51758abe4a6a45887
keekss <keeksslss@gmail.com>
2020-11-18 22:22:20 -1000

    Fixed ESLint error

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Header } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
