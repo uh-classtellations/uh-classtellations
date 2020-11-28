06b738942b714792cd060a876945ddd3574e7b7d
alison-t <45301512+alison-t@users.noreply.github.com>
2020-11-28 13:35:27 -1000

    create private data file, more default accounts, and edit mongo for using private data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Courses } from '../../api/course/Course';
 
 /* eslint-disable no-console */
 
@@ -15,4 +16,26 @@ if (Stuffs.collection.find().count() === 0) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
   }
+}
+
+/** Defines new course for user. Error if exact course already exists. */
+function addCourse({ semester, name, credits, status, grade, owner }) {
+  console.log(`Defining course ${name}`);
+  // Create the profile.
+  Courses.collection.insert({ semester, name, credits, status, grade, owner });
+}
+
+/**
+ * If the loadAssetsFile field in settings.development.json is true, then load the data in private/data.json.
+ * This approach allows you to initialize your system with large amounts of data.
+ * Note that settings.development.json is limited to 64,000 characters.
+ * We use the "Assets" capability in Meteor.
+ * For more info on assets, see https://docs.meteor.com/api/assets.html
+ * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
+ */
+if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 7)) {
+  const assetsFileName = 'data.json';
+  console.log(`Loading data from private/${assetsFileName}`);
+  const jsonData = JSON.parse(Assets.getText(assetsFileName));
+  jsonData.courses.map(course => addCourse(course));
 }
