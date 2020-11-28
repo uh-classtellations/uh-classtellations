62b815d4a2a9e658b7216bfa3e41620eb78ef71d
alison-t <45301512+alison-t@users.noreply.github.com>
2020-11-28 13:10:50 -1000

    format semester for sort compatability

--- app/imports/ui/components/AddCourse.jsx
+++ app/imports/ui/components/AddCourse.jsx
@@ -11,8 +11,8 @@ import { Courses } from '../../api/course/Course';
 const formSchema = new SimpleSchema({
   semester: {
     type: String,
-    allowedValues: ['Spring 2018', 'Fall 2018', 'Spring 2019', 'Fall 2019', 'Spring 2020', 'Fall 2020'],
-    defaultValue: 'Fall 2020',
+    allowedValues: ['2016 Spring', '2016 Fall', '2017 Spring', '2017 Fall', '2018 Spring', '2018 Fall', '2019 Spring', '2019 Fall', '2020 Spring', '2020 Fall', '2021 Spring'],
+    defaultValue: '2020 Fall',
   },
   name: {
     type: String,
